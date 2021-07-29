0cad19d82563ab0ed50ba8150d14436467ad7286
pmpeter <pmpeter@msp.amadeus.net>
2021-07-29 19:00:17 +0200

    Fix: index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
+		<HashRouter/>
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
